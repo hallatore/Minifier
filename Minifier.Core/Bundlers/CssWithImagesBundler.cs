2346e36be7c0423a5d29760b4cbba8cda49fbc4e
Tore Lervik <tore.lervik@bekk.no>
2011-09-23 19:11:52 +0200

    endret hvordan bilder blir funnet i cssBundler

--- Minifier.Core/Bundlers/CssWithImagesBundler.cs
+++ Minifier.Core/Bundlers/CssWithImagesBundler.cs
@@ -1,46 +1,15 @@
-using System;
-using System.IO;
-using System.Text.RegularExpressions;
 using System.Web;
-using System.Web.Hosting;
 using Microsoft.Web.Optimization;
 
 namespace Lervik.Minifier.Core.Bundlers
 {
     public class CssWithImagesBundler : CssBundler
     {
-        private static readonly Regex url = new Regex(@"url\((([^\)]*)\?embed)\)", RegexOptions.Singleline);
-        private const string format = "url(data:image/{0};base64,{1})";
-
         public override void Process(BundleResponse bundle)
         {
             HttpContext.Current.Response.Cache.SetLastModifiedFromFileDependencies();
-
             base.Process(bundle);
-
-            string reference = HttpContext.Current.Request.Path.Replace("/css", "/");
-
-            // When publishing the bundler can be called from "/" causing image reference to be invalid.
-            if (reference == "/")
-                reference = "/Content/";
-
-            foreach (Match match in url.Matches(bundle.Content))
-            {
-                var file = new FileInfo(HostingEnvironment.MapPath(reference + match.Groups[2].Value));
-                if (file.Exists)
-                {
-                    string dataUri = GetDataUri(file);
-                    bundle.Content = bundle.Content.Replace(match.Value, dataUri);
-                    HttpContext.Current.Response.AddFileDependency(file.FullName);
-                }
-            }
-        }
-
-        private string GetDataUri(FileInfo file)
-        {
-            byte[] buffer = File.ReadAllBytes(file.FullName);
-            string ext = file.Extension.Substring(1);
-            return string.Format(format, ext, Convert.ToBase64String(buffer));
+            EmbedImages.Process(bundle);
         }
     }
 }
---
+++ Minifier.Core/EmbedImages.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Web;
+using Microsoft.Web.Optimization;
+
+namespace Lervik.Minifier.Core
+{
+    internal class EmbedImages
+    {
+        private const string format = "url(data:image/{0};base64,{1})";
+        private static readonly Regex url = new Regex(@"url\((([^\)]*)\?embed)\)", RegexOptions.Singleline);
+
+        public static void Process(BundleResponse bundle)
+        {
+            foreach (Match match in url.Matches(bundle.Content))
+            {
+                var file = GetFileInfo(match.Groups[2].Value, bundle.Files);
+                if (file != null)
+                {
+                    string dataUri = GetDataUri(file);
+                    bundle.Content = bundle.Content.Replace(match.Value, dataUri);
+                    HttpContext.Current.Response.AddFileDependency(file.FullName);
+                }
+            }
+        }
+
+        private static FileInfo GetFileInfo(string filename, IEnumerable<FileInfo> files)
+        {
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file.Directory + "/" + filename);
+                if (fileInfo.Exists)
+                    return fileInfo;
+            }
+
+            return null;
+        }
+
+        private static string GetDataUri(FileInfo file)
+        {
+            byte[] buffer = File.ReadAllBytes(file.FullName);
+            string ext = file.Extension.Substring(1);
+            return string.Format(format, ext, Convert.ToBase64String(buffer));
+        }
+    }
+}
